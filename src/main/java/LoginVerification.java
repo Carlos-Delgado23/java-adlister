fcb82f1a5de190890a7f9641130378948028fc4a
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-02-29 22:31:10 -0600

    serparating concerns

--- src/main/java/LoginVerification.java
+++ src/main/java/LoginVerification.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,12 +8,18 @@ import java.io.IOException;
 @WebServlet("/login")
 public class LoginVerification extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
         if (request.getMethod().equalsIgnoreCase("post")) {
             String username = request.getParameter("username");
             String password = request.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
-                response.sendRedirect("./profile.jsp");
+                response.sendRedirect("/profile");
             }
         }
     }
